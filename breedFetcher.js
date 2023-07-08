c38dde0a90350d57cc62a5a2ec4b61d18d3d721e
jasmine hilton <jasminehilton1@live.com>
2023-07-08 00:33:58 +0000

    added the index.js and the file works

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,24 @@
 const request = require("request");
 
-let searchQuery = process.argv.slice(2);
-searchQuery = searchQuery[0];
-
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${searchQuery}`,
-  function (error, _, body) {
-    if (error) {
-      console.error("error:", error); // Print the error if one occurred
-      return;
-    }
-    if (body) {
-      const data = JSON.parse(body);
-      if (data.length === 0) {
-        console.log(
-          `Sorry, the ${searchQuery} cat breed has not been discovered yet.`
-        );
-        return;
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    function (error, _, body) {
+      if (error) {
+        return callback(error, null);
       }
-      console.log(data[0].description);
+      if (body) {
+        const data = JSON.parse(body);
+        if (data.length === 0) {
+          return callback(
+            `Sorry, the ${breedName} cat breed has not been discovered yet.`,
+            null
+          );
+        }
+        return callback(null, data[0].description);
+      }
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
